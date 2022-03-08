046fecf2cfbbebbbaebb027519b02ec2dd19c9a9
904211508 <904211508@L815WD150045.edu.cbe.ab.ca>
2022-03-08 11:04:13 -0700

    update

--- Chapter 6/src/PrimeNum.java
+++ Chapter 6/src/PrimeNum.java
@@ -1,5 +1,14 @@
-import java.awt.EventQueue;
+/*
+Program: PrimeNum.java          Last Date of this Revision: March 8th,2022
 
+Purpose: An application that allows the user to find out whether or not a number is prime.
+
+Author: Tyler Grewal 
+School: CHHS
+Course: Computer Programming 20 */
+
+//importing items
+import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -10,7 +19,7 @@ import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 
-public class PrimeNum {
+public class PrimeNum { //start of class
 
 	private JFrame frame;
 	private JTextField inpT;
@@ -45,23 +54,23 @@ public class PrimeNum {
 	
 	
 	
-	public static boolean isPrime( int x)
+	public static boolean isPrime( int x) //sets number isPrime as x
 	{
-		int divisor = 1;
+		int divisor = 1; //divisor is 1
 		
 		do
 		{
-			divisor = divisor  + 1;
+			divisor = divisor  + 1; //add one to the divisor while x % 0 isn't true. Which means it'll keep adding until it divides properly.
 
 		}while(( x % divisor) != 0);
 
-		if(divisor == x)
+		if(divisor == x) //if the divisor was equal to the number inputed, then it's prime since the number is divisible by itself before anything else
 		{
 			return true;
 		}
 		else
 		{
-			return false;
+			return false; //if the divisor isn't equal to the number, it means it isn't prime because there's another number that allows for it to be divided.
 		}
 	}
 	
@@ -71,13 +80,13 @@ public class PrimeNum {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() { //GUI
 		frame = new JFrame();
 		frame.setBounds(100, 100, 535, 365);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
+		JPanel panel = new JPanel(); 
 		panel.setBounds(10, 11, 499, 304);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
@@ -88,13 +97,13 @@ public class PrimeNum {
 		panel.add(inpT);
 		inpT.setColumns(10);
 		
-		JLabel dis = new JLabel("is Prime:");
+		JLabel dis = new JLabel("The number is prime"); //label that expresses a statement to be either true or false
 		dis.setHorizontalAlignment(SwingConstants.CENTER);
 		dis.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 16));
 		dis.setBounds(61, 175, 376, 54);
 		panel.add(dis);
 		
-		JLabel lblNewLabel = new JLabel("Enter a Number");
+		JLabel lblNewLabel = new JLabel("Enter a Number"); //user input
 		lblNewLabel.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 16));
 		lblNewLabel.setBounds(100, 23, 135, 14);
 		panel.add(lblNewLabel);
@@ -105,23 +114,19 @@ public class PrimeNum {
 		
 		
 		
-		JButton subm = new JButton("Submit");
+		JButton subm = new JButton("Submit"); //to start isPrime
 		subm.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 16));
 		subm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
-				int userNum = Integer.parseInt(inpT.getText());
+				int userNum = Integer.parseInt(inpT.getText()); //gets text back
 				
 				
 				
-				dis.setText(Boolean.toString(isPrime(userNum)));
+				dis.setText(Boolean.toString(isPrime(userNum))); //converts boolean to text and displays it
 			}
 		});
 		subm.setBounds(178, 81, 143, 39);
 		panel.add(subm);
-		
-
-		
-		
 	}
-}
+} //end of class
