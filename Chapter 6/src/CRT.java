9c9a0460531aa8fe7534f0bcd768208cfad5fc7a
904211508 <904211508@L815WD150045.edu.cbe.ab.ca>
2022-03-14 10:15:04 -0600

    Chap 6 done

---
+++ Chapter 6/src/CRT.java
@@ -0,0 +1,17 @@
+public class CRT 
+{
+}
+
+	/* Chapter 6 CRT
+	2.  Method body is where all of the code takes place; it implements a method. A method declaration is called depending on what the user wants; it contains name, access level, return type, and parameters.
+	3.  An access modifier is the keyword that determines the access level of a method; changing this also changes the access level.
+	4.  Visibility is another word used to describe the access level of a method. 
+	5.  Var 1 and 2 are local to main method, and var 3 and 4 are local to method 1.
+	6.
+		a. public static int getVowels (String val)
+		b. public static int ExtractDigit (Int val)
+		c. public static String insertString (Sring val)
+	7. 
+		a. It distinguishes it based off of the name and order of its parameters.
+	b. Two methods can have the same name, as long as the parameters are different; this is called method overloading. */
+
