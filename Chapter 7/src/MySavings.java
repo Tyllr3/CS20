aad5972d790d98e36ee93a5a874ad684d503c31e
904211508 <904211508@L815WD220032.edu.cbe.ab.ca>
2022-03-30 10:21:04 -0600

    update

--- Chapter 7/src/MySavings.java
+++ Chapter 7/src/MySavings.java
@@ -1,41 +1,43 @@
-import java.awt.EventQueue;
+/* woo yas got permission to skip the GUI for this
+ 
+Program: MySavings.java          Last Date of this Revision: March 30th,2022
 
-import javax.swing.JFrame;
+Purpose: An application that displays a menu of choices for entering different amounts into a piggy bank that can display the amount too
 
-public class MySavings {
-
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MySavings window = new MySavings();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+Author: Tyler Grewal 
+School: CHHS
+Course: Computer Programming 20 */
+ 
+import java.util.Scanner;
+public class MySavings 
+{
+	private static double amount;
+	private static double Newamount;
+	
+	public static void main(String[] args)
+	{
+			Scanner myObj = new Scanner(System.in);
+			System.out.println("1. Show total in bank\n2. Add a penny\n3. Add a nickel\n4. Add a dime\n5. Add a quarter\n6. Take money out of bank\nEnter 0 to quit\nEnter your choice:"); //user input
+			int number = myObj.nextInt(); //recording input
+			
+		do
+		{
+			
+			
+			switch (number) //starts a switch statement
+			{
+			case 0: System.out.println("Thanks for using this program!");
+			case 1: System.out.println("Your amount is " + Newamount);
+			case 2: Newamount = amount + 0.01;
+			case 3: Newamount = amount + 0.05;
+			case 4: Newamount = amount + 0.10;
+			case 5: Newamount = amount + 0.25;
+			case 6: 
+			 
 			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public MySavings() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */ 
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			myObj.close();
+			
+		}while(number != 0);
 	}
 
 }
---
+++ Chapter 7/src/PiggyBank.java
@@ -0,0 +1,14 @@
+
+public class PiggyBank 
+{
+
+	//Constructor method should be created
+	
+	//Create a method called void addCoin(double cents)
+	
+	
+	//Create a method called double giveTotal()
+	
+	
+	//Create a method called void removeCoins(double amt)
+}
