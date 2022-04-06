53441a1d171cf6cadc6ed137d07679efef31946a
904211508 <904211508@L815WD220032.edu.cbe.ab.ca>
2022-04-06 09:56:37 -0600

    update

---
+++ Chapter 9/src/StudentRoster.java
@@ -0,0 +1,17 @@
+/* Program: StudentRoster.java          Last Date of this Revision: April 6th,2022
+
+Purpose: An application that prompts the user for the number of students in a class, their names and lists the names in an array
+Author: Tyler Grewal 
+School: CHHS
+Course: Computer Programming 20 */
+
+public class StudentRoster 
+{
+
+	public static void main(String[] args) 
+	{
+	
+
+	}
+
+}
--- Chapter 9/src/bruh.java
+++
@@ -1,8 +0,0 @@
-public class bruh {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
