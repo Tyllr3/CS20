27721bb1bb532d8256c871a6e57d15a470affd1e
904211508 <904211508@L815WD220032.edu.cbe.ab.ca>
2022-04-11 10:20:43 -0600

    update

--- Chapter 9/src/Squares.java
+++ Chapter 9/src/Squares.java
@@ -1,3 +1,9 @@
+/* Program: Squares.java          Last Date of this Revision: April 11th,2022
+
+Purpose: An application that stores he square of an element's index and displays them.
+Author: Tyler Grewal 
+School: CHHS
+Course: Computer Programming 20 */
 
 public class Squares {
 
