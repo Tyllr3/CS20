deb95c01e1bdf2f069975fa3f2ca6f2171af77e8
904211508 <904211508@L815WD220032.edu.cbe.ab.ca>
2022-04-13 10:22:34 -0600

    update

--- Chapter 9/src/Reverse.java
+++ Chapter 9/src/Reverse.java
@@ -1,12 +1,11 @@
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
 import javax.swing.JButton;
-import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.Font;
 
 public class Reverse 
 {
@@ -57,21 +56,41 @@ public class Reverse
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lbl = new JLabel("New label");
-		lbl.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl.setBounds(10, 141, 414, 109);
-		frame.getContentPane().add(lbl);
+		JTextArea lbl = new JTextArea();
+		lbl.setFont(new Font("Monospaced", Font.ITALIC, 33));
+		lbl.setBounds(10, 148, 394, 69);
 		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 414, 239);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		panel.add(lbl);
 		
 		
 		JButton btnNewButton = new JButton("Count Down !");
+		btnNewButton.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 12));
+		btnNewButton.setBounds(136, 50, 136, 51);
+		panel.add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
-				//lbl.setText(number[i]);
+				int[]number = new int[10];
+				
+				for (int i =0; i <= 9; i++)
+				{
+					number[i] = i;
+					
+				}
+				
+				
+				for (int i =9; i >= 0; i--)
+				{
+					number[i] = i;
+					String el = Integer.toString(number[i]);
+					lbl.append(el + " ");
+				}
 			}
 		});
-		btnNewButton.setBounds(167, 75, 105, 35);
-		frame.getContentPane().add(btnNewButton);
 	}
 }
