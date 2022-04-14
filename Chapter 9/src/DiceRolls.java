77821a2c543927e80ab638676ef32dfe18cf633d
904211508 <904211508@L815WD220032.edu.cbe.ab.ca>
2022-04-14 11:10:08 -0600

    update

---
+++ Chapter 9/src/DiceRolls.java
@@ -0,0 +1,106 @@
+/* Program: DiceRolls      Last Date of this Revision: April 14h,2022
+
+Purpose: An application that promps the user for the amount of rolls they would like to do with 3 dice, and then displays the outcomes.
+Author: Tyler Grewal 
+School: CHHS
+Course: Computer Programming 20 */
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class DiceRolls { //start of class
+
+	private JFrame frame;
+	private JTextField num;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) 
+	{
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DiceRolls window = new DiceRolls();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	/**
+	 * Create the application.
+	 */
+	public DiceRolls() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 414, 239);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JTextArea area = new JTextArea();
+		area.setFont(new Font("MS Gothic", Font.PLAIN, 13));
+		area.setBounds(177, 0, 227, 239);
+		panel.add(area);
+		
+		JButton btn = new JButton("click !");
+		btn.setFont(new Font("Wide Latin", Font.PLAIN, 11));
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				int numRolls = Integer.parseInt(num.getText()); //starts the code when the button is pushed 
+				int[] outcomes = new int [19];
+				int outcome;
+				
+				for (int roll = 0; roll < numRolls; roll++) //rolls until the amount of rolls = the number of rolls the input requested 
+				{
+					outcome = (int)(6 * Math.random() + 1) + (int) (6 * Math.random() +1) + (int) (6 * Math.random() +1); // adds the 3 dice outcomes together 
+					outcomes[outcome] +=1;
+				}
+				
+				for (int i = 3; i <=18; i ++) //between 3-18, only possible for 3 dice
+				{
+					area.append(i + ":" + outcomes[i] +"\n"); //displays the outcomes in a line
+				}
+			}
+		});
+		btn.setBounds(51, 102, 89, 23);
+		panel.add(btn);
+		
+		num = new JTextField();
+		num.setFont(new Font("Sitka Subheading", Font.PLAIN, 12));
+		num.setHorizontalAlignment(SwingConstants.CENTER);
+		num.setBounds(41, 50, 113, 35);
+		panel.add(num);
+		num.setColumns(10);
+		
+		JLabel lbl = new JLabel("How Many Rolls?");
+		lbl.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 13));
+		lbl.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl.setBounds(10, 11, 171, 30);
+		panel.add(lbl);
+	} //end of class 
+}
--- Chapter 9/src/Reverse.java
+++ Chapter 9/src/Reverse.java
@@ -1,3 +1,10 @@
+/* Program: Reverse.java          Last Date of this Revision: April 13th,2022
+
+Purpose: An application that displays a count down.
+Author: Tyler Grewal 
+School: CHHS
+Course: Computer Programming 20 */
+
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JButton;
@@ -7,7 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import java.awt.Font;
 
-public class Reverse 
+public class Reverse //start of class
 {
 
 	private JFrame frame;
@@ -73,24 +80,24 @@ public class Reverse
 		btnNewButton.setBounds(136, 50, 136, 51);
 		panel.add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) 
+			public void actionPerformed(ActionEvent e) //button is pushed
 			{
-				int[]number = new int[10];
+				int[]number = new int[10]; // 10 indexes
 				
-				for (int i =0; i <= 9; i++)
+				for (int i =0; i <= 9; i++) //correct order 
 				{
-					number[i] = i;
+					number[i] = i; //stores 
 					
 				}
 				
 				
-				for (int i =9; i >= 0; i--)
+				for (int i =9; i >= 0; i--) //allows for numbers to reverse 
 				{
 					number[i] = i;
-					String el = Integer.toString(number[i]);
-					lbl.append(el + " ");
+					String el = Integer.toString(number[i]); //converts to string 
+					lbl.append(el + " "); //displays the numbers in a reverse order 
 				}
 			}
 		});
 	}
-}
+} //end of class
